c70c8525c1109a8418745af2279a52a613a6911f
soheeen96 <gmldms6323@gmail.com>
2022-12-20 13:54:43 +0900

    Create project

---
+++ webpack.config.js
@@ -0,0 +1,48 @@
+//import
+const path = require('path')
+const HtmlPlugin = require('html-webpack-plugin')
+const CopyPlugin = require('copy-webpack-plugin');
+
+// export
+module.exports = {
+    //파일을 읽기 시작하는 진입점 설정
+    entry:'./js/main.js', 
+
+    //결과물(번들)을 변환하는 설정 (절대경로)
+    output:{ 
+        // path: path.resolve(__dirname, 'dist'), //1, 2의 경로를 합쳐줌
+        // filename: 'main.js',
+        clean: true
+    },
+
+    module: {
+        rules :[{
+            test:  /\.s?css$/,
+            use: [
+                'style-loader',
+                'css-loader',
+                'postcss-loader',
+                'sass-loader'
+            ]
+        },{
+           test: /\.js$/,
+           use: [
+            'babel-loader'
+           ] 
+        }]
+    },
+
+    // 번들링 후 결과물의 처리 방식 등 다양한 플러그인을 설정
+    plugins: [
+        new HtmlPlugin({
+            template: './index.html'
+        }),
+
+        
+        new CopyPlugin({
+            patterns: [
+              { from: "static"}
+            ],
+          }),
+    ]
+}
